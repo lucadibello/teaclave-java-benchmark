be18162bb9625ba8fa84bdd54635648f96ec87d2
Luca Di Bello <lucadibello@proton.me>
2025-10-28 22:05:24 +0000

    updated run.sh script

--- teaclave/run.sh
+++ teaclave/run.sh
@@ -4,4 +4,4 @@ mvn -Pnative clean package
 
 ENABLE_OCCULUM=false
 
-OCCLUM_RELEASE_ENCLAVE=$ENABLE_OCCULUM $JAVA_HOME/bin/java -cp host/target/host-1.0-SNAPSHOT-jar-with-dependencies.jar:enclave/target/enclave-1.0-SNAPSHOT-jar-with-dependencies.jar com.benchmark.teaclave.host.Main
+OCCLUM_RELEASE_ENCLAVE=$ENABLE_OCCULUM sudo $JAVA_HOME/bin/java -cp host/target/host-1.0-SNAPSHOT-jar-with-dependencies.jar:enclave/target/enclave-1.0-SNAPSHOT-jar-with-dependencies.jar com.benchmark.teaclave.host.Main
